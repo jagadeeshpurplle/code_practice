f47e2db439ee408069626fdbeb4261392a64d96d
jagadeesh <jagadeesh@pharmeasy.in>
2020-04-04 17:10:20 +0530

    longest palidrum substring

---
+++ src/codeChallenge30Days/LongestPalindrum.java
@@ -0,0 +1,36 @@
+package codeChallenge30Days;
+
+public class LongestPalindrum {
+
+	public static int resultStart;
+	public static int resultLength;
+
+	public static void main(String[] args) {
+		System.out.println(getLongestPalindrum("subbaraooar"));
+	}
+
+	public static String getLongestPalindrum(String s) {
+		int size = s.length();
+		if (size < 2)
+			return s;
+		for (int start = 0; start < s.length() - 1; start++) {
+			getExpandRange(s, start, start); // if string size is even
+			getExpandRange(s, start, start + 1); // if string size is add
+		}
+
+		return s.substring(resultStart, resultStart + resultLength);
+	}
+
+	private static void getExpandRange(String s, int start, int end) {
+
+		while (start >= 0 && end <= s.length() - 1 && s.charAt(start) == s.charAt(end)) {
+			start--;
+			end++;
+		}
+
+		if (resultLength < end - start - 1) {
+			resultStart = start + 1;
+			resultLength = end - start - 1;
+		}
+	}
+}
